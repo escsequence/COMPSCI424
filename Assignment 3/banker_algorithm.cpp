f3ac8800405b033578dc3367ac189696c82a5229
James Johnston <jamestheprogrammer@outlook.com>
2021-03-30 14:04:04 -0500

    Assignment 3 - Comments.

--- Assignment 3/banker_algorithm.cpp
+++ Assignment 3/banker_algorithm.cpp
@@ -15,11 +15,23 @@ int banker_algorithm::run() {
 }
 
 int banker_algorithm::run_manual() {
+
+  // We keep processing getting input values
   while(manual_process_inputs) {
+
+    // Entry string from user
     std::string entry;
+
+    // Formatting
     std::cout << "Command: ";
+
+    // Get the line from the command
     getline(std::cin, entry);
+
+    // Get the current command (parsing it from our static function)
     ba_command::command current = ba_command::parse(entry);
+
+    // Identify what wer are trying to do..
     switch (current.action) {
       case ba_command::CA_REQUEST:
         break;
@@ -33,11 +45,14 @@ int banker_algorithm::run_manual() {
         std::cout << "Invalid input. Please either try \'request\', \'release\', or \'end\'" << std::endl;
     }
   }
+
+  // Return control back to the original run
   return 0;
 }
 
 
 
 int banker_algorithm::run_auto() {
+  // Yet to be developed.
   return 0;
 }
