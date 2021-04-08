ffda1ad7275272e3d4b8fafd47e03ffcbe987cc5
James Johnston <jamestheprogrammer@outlook.com>
2021-04-08 16:06:05 -0500

    Assignment 3 - Fix for trim build error.

--- Assignment 3/ba.h
+++ Assignment 3/ba.h
@@ -29,7 +29,7 @@
 static inline std::string &ltrim(std::string &str) {
     str.erase(str.begin(), std::find_if(str.begin(), str.end(),
             std::not1(std::ptr_fun<int, int>(std::isspace))));
-    return s;
+    return str;
 }
 
 /**
@@ -41,7 +41,7 @@ static inline std::string &ltrim(std::string &str) {
 static inline std::string &rtrim(std::string &str) {
     str.erase(std::find_if(str.rbegin(), str.rend(),
             std::not1(std::ptr_fun<int, int>(std::isspace))).base(), str.end());
-    return s;
+    return str;
 }
 
 /**
